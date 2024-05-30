cecb629850c9de5f950d48b62ac4c71e2a7a6d68
Josh V <joshkv@gmail.com>
2024-05-29 19:19:58 -0700

    next.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component }  from 'react';
 import { HashRouter, Routes, Route, Link } from "react-router-dom";
-import CTA from './mailto/mailto';
-import Geocoder from './mailto/components/geocoder';
-import Feed from './mailto/feed'
+import CTA from './app/mailto/mailto';
+import Geocoder from './app/mailto/components/geocoder';
+import Feed from './app/mailto/feed'
 
 
 
