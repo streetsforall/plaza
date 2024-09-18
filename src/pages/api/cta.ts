16ff31a0d77e6d9ad0638daf985e90afbb37e2ea
Josh V <joshkv@gmail.com>
2024-09-18 13:41:21 -0700

    data

--- src/pages/api/cta.ts
+++ src/pages/api/cta.ts
@@ -7,6 +7,7 @@ export default async function handler(req, res) {
   if (req.method === 'GET') {
     try {
       const data = await getMailchimp();
+      console.log(data)
       res.status(200).json(data);
     } catch (error) {
       console.error('Error fetching Mailchimp data:', error);
