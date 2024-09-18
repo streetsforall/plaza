a795fe209659767c049cf5d01e706bcca51e5980
Josh V <joshkv@gmail.com>
2024-09-18 12:40:48 -0700

    different api structure

---
+++ src/pages/api/cta.tsx
@@ -0,0 +1,24 @@
+
+import {getMailchimp} from "../../app/mailto/helpers/mailchimp"
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+
+    if (req.method === "GET") {
+        try {
+            const mailchimpData = await getMailchimp()
+
+            res.status(200).send(mailchimpData)
+
+        } catch (error: any) {
+            if (error instanceof Error) {
+                console.error(`An error occurred retrieving emails: ${error.message}`);
+            }
+
+            res.status(500).send("An error occurred retrieving emails");
+        }
+    }
+}
