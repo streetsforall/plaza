a982c3271284ab2fdef6da17f8570b0202e08869
Josh V <joshkv@gmail.com>
2024-09-18 11:51:37 -0700

    fallback

--- src/app/api/cta/route.tsx
+++ src/app/api/cta/route.tsx
@@ -5,6 +5,13 @@ import {getMailchimp} from "../../mailto/helpers/mailchimp"
 
 export async function GET(request: Request) {
   const mailchimpData = await getMailchimp()
+  if (!mailchimpData) {
+    return NextResponse.json({
+      notFound: true,
+    })
+  }
+  return NextResponse.json(mailchimpData)
+
 
   // const data = await mailchimpData.json()
 
