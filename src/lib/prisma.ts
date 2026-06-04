d2bbeaf18a68773b5700f5685ede5096a23540ca
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2026-06-03 20:40:23 -0700

    Initialize Prisma

---
+++ src/lib/prisma.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "../../generated/prisma/client";
+
+// Create global Prisma client so only one instance is created
+// Helps resolve issues with hot reloading that can occur with Next.js in development mode
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient; 
+}; 
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient(); 
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma; 
+
+export default prisma; 
