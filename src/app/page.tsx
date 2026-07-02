6ddba67819c57984bae64a20c3f6ae2293594815
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2026-07-01 17:24:54 -0700

    Fix home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 const App = () => {
   return (
-    <div className="font-mono text-xs">
+    <div className="bg-sfa-tan min-h-screen p-4 font-mono text-xs">
       <p className="mb-4">
         Welcome to the{' '}
         <a href="https://www.streetsforall.org/">Streets for All</a> Plaza
