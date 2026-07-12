6672f6da5a9c086e5eab6ded3bc10a796e97ef41
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2026-07-11 23:17:14 -0700

    Rename password environment variable

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -14,7 +14,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [
     Credentials({
       authorize: async (credentials) => {
-        if (credentials.password === process.env.LOGIN) {
+        if (credentials.password === process.env.AUTH_PASSWORD) {
           return { id: '' };
         }
         return null;
