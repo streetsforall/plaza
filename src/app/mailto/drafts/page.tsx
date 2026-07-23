73bc3a792bf51478839b82d485ee2e24ca645f2d
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2026-07-22 23:03:44 -0700

    Move list page to server rendering

--- src/app/mailto/drafts/page.tsx
+++ src/app/mailto/drafts/page.tsx
@@ -1,21 +1,8 @@
-'use client';
-
-import { useState, useEffect } from 'react';
-import { Prisma } from 'generated/prisma/client';
+import Link from 'next/link';
 import { getAllEmailTemplates } from '../helpers/db';
 
-function Drafts() {
-  const [drafts, setDrafts] = useState<Prisma.EmailTemplateModel[]>();
-
-  useEffect(() => {
-    async function loadEmailTemplates() {
-      const emailTemplates = await getAllEmailTemplates();
-
-      setDrafts(emailTemplates);
-    }
-
-    loadEmailTemplates();
-  }, []);
+export default async function Page() {
+  const emailTemplates = await getAllEmailTemplates();
 
   return (
     <div className="container m-auto min-h-screen">
@@ -27,7 +14,7 @@ function Drafts() {
 
       <div className="mx-auto border-2 border-black bg-white">
         <table className="border-separate border-spacing-0">
-          <thead className="sticky top-0 bg-white">
+          <thead className="sticky top-0 z-10 bg-white">
             <tr>
               <th className="border-b-2 border-black px-4 py-2 text-left">
                 Email subject
@@ -44,30 +31,45 @@ function Drafts() {
             </tr>
           </thead>
           <tbody>
-            {drafts &&
-              drafts.map((draft) => {
+            {emailTemplates &&
+              emailTemplates.map((template) => {
                 // Remove # symbol
-                const hash = draft.url.substring(1);
+                const hash = template.url.substring(1);
 
                 return (
                   <tr
-                    key={draft.id}
+                    key={template.id}
                     className="group cursor-pointer hover:bg-black hover:text-white"
-                    onClick={() => (location.href = `/mailto/${hash}`)}
                     aria-label="Load"
                   >
-                    <td className="w-1/2 max-w-0 overflow-hidden px-4 py-2 text-ellipsis whitespace-nowrap group-not-last:border-b-2">
-                      {decodeURIComponent(draft.subject || '-')}
+                    <td className="relative w-1/2 max-w-0 overflow-hidden px-4 py-2 text-ellipsis whitespace-nowrap group-not-last:border-b-2">
+                      <Link
+                        href={`/mailto/${hash}`}
+                        className="absolute inset-0"
+                      />
+                      {decodeURIComponent(template.subject || '-')}
                     </td>
-                    <td className="w-1/2 max-w-0 overflow-hidden px-4 py-2 text-ellipsis whitespace-nowrap group-not-last:border-b-2">
-                      {decodeURIComponent(draft.actionable?.header || '-')}
+                    <td className="relative w-1/2 max-w-0 overflow-hidden px-4 py-2 text-ellipsis whitespace-nowrap group-not-last:border-b-2">
+                      <Link
+                        href={`/mailto/${hash}`}
+                        className="absolute inset-0"
+                      />
+                      {decodeURIComponent(template.actionable?.header || '-')}
                     </td>
-                    <td className="px-4 py-2 font-mono text-sm group-not-last:border-b-2">
+                    <td className="relative px-4 py-2 font-mono text-sm group-not-last:border-b-2">
+                      <Link
+                        href={`/mailto/${hash}`}
+                        className="absolute inset-0"
+                      />
                       {hash}
                     </td>
-                    <td className="px-4 py-2 group-not-last:border-b-2">
-                      {draft.time
-                        ? new Date(draft.time).toISOString().slice(0, 10)
+                    <td className="relative px-4 py-2 group-not-last:border-b-2">
+                      <Link
+                        href={`/mailto/${hash}`}
+                        className="absolute inset-0"
+                      />
+                      {template.time
+                        ? new Date(template.time).toISOString().slice(0, 10)
                         : ''}{' '}
                     </td>
                   </tr>
@@ -79,5 +81,3 @@ function Drafts() {
     </div>
   );
 }
-
-export default Drafts;
