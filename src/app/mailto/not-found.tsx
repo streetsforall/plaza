c64455b1d1a3381fbdbc3868c004cdf2b5c11f8f
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2026-07-22 23:52:14 -0700

    Create custom not found page

---
+++ src/app/mailto/not-found.tsx
@@ -0,0 +1,13 @@
+export default function NotFound() {
+  return (
+    <div className="container m-auto min-h-screen">
+      <div className="flex min-h-screen items-center">
+        <div className="mx-auto w-max border-2 border-black bg-white p-8">
+          <h1 className="text-2xl font-bold">Not Found</h1>
+
+          <p>Sorry, but the page you&apos;re looking for doesn&apos;t exist.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
