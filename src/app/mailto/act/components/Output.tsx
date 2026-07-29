40c70d5d75c8c2a2525f6966169c799448d89e7e
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2026-07-28 18:41:23 -0700

    Debounce address search

--- src/app/mailto/act/components/Output.tsx
+++ src/app/mailto/act/components/Output.tsx
@@ -33,24 +33,33 @@ export default function Output({
   const [status, setStatus] = useState<string>('Waiting for address');
 
   // Address lookup
-  const [addressSearch, setAddressSearch] = useState<string>();
+  const [addressQuery, setAddressQuery] = useState<string>('');
+  const [debouncedQuery, setDebouncedQuery] = useState<string>('');
   const [addressResults, setAddressResults] = useState<any[]>();
 
-  // Update list of addresses when field changes
+  // Wait for pause
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedQuery(addressQuery);
+    }, 500);
+    return () => clearTimeout(timeoutId);
+  }, [addressQuery]);
+
+  // Search for address
   useEffect(() => {
     async function getCoords() {
       const body = {
-        string: addressSearch,
+        string: debouncedQuery,
       };
 
       const jsonData = await geo(body);
       setAddressResults(jsonData);
     }
 
-    if (addressSearch) {
+    if (debouncedQuery) {
       getCoords();
     }
-  }, [addressSearch]);
+  }, [debouncedQuery]);
 
   // Geotargeted information
   const [district, setDistrict] = useState<any>();
@@ -229,9 +238,7 @@ export default function Output({
               id="address-query"
               className="w-full rounded border-2 border-stone-300 px-4 py-3 leading-none text-stone-600"
               placeholder="Enter address here"
-              onChange={(e) =>
-                setAddressSearch(e.target.value + ', California')
-              }
+              onChange={(e) => setAddressQuery(e.target.value + ', California')}
             />
             {addressResults?.length ? (
               <ul className="w-full overflow-hidden rounded border-2 border-stone-300">
