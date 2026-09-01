f8eb1fd34104f6a2ee55ebf42e34e7fd2a91506a
Jonathan Chue <5141427+jchue@users.noreply.github.com>
2026-08-31 17:43:24 -0700

    Get neighborhood councils from API

--- src/app/edit/components/ContactLibrary.tsx
+++ src/app/edit/components/ContactLibrary.tsx
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Checkbox, Dialog, Tabs } from 'radix-ui';
 import { Icon } from '@iconify/react';
 import { geoLoader } from '../../helpers/geo';
-import neighborhoods from '../../data/LA_Neighborhood_Councils.json';
 import metro from '../../data/metro.json';
 import cds from '../../data/LA_City_Council_Districts.json';
 //import assembly from '../data/CA_Assembly_Districts.json';
 import senate from '../../data/CA_Senate_Districts.json';
 import Santa_Monica from '../../data/Santa_Monica.json';
+import { getNeighborhoodCouncils } from '@/app/helpers/contacts';
 
 interface datafeatures {
   OBJECTID: number;
@@ -23,31 +23,57 @@ interface datafeatures {
   SERVICE_RE: string;
 }
 
+interface Category {
+  id: string;
+  label: string;
+  data:
+    | GeoJSON.Feature[]
+    | {
+        updatedAt: Date;
+        contacts: Contact[];
+      };
+  updatedAt?: Date;
+}
+
+interface Contact {
+  id: string;
+  title: string;
+  name: string;
+  primaryEmail: string;
+  secondaryEmail: string;
+}
+
 export default function ContactLibrary({ recipients, setRecipients }) {
   const [areDeputiesShown, setAreDeputiesShown] = useState(false);
+  const [categories, setCategories] = useState<Category[]>([]);
 
-  const categories = [
-    {
-      id: 'nc',
-      label: 'LA Neighborhood Councils',
-      data: neighborhoods.features,
-    },
-    /*{
+  useEffect(() => {
+    async function loadContacts() {
+      const neighborhoodCouncils = await getNeighborhoodCouncils();
+
+      setCategories([
+        {
+          id: 'nc',
+          label: 'LA Neighborhood Councils',
+          data: neighborhoodCouncils.contacts,
+          updatedAt: neighborhoodCouncils.updatedAt,
+        },
+        /*{
         id: 'cd',
         label: 'LA City Council',
         data: cds.features,
       },*/
-    {
-      id: 'metro',
-      label: 'Metro',
-      data: metro.features,
-    },
-    {
-      id: 'santamonica',
-      label: 'Santa Monica',
-      data: Santa_Monica.features,
-    },
-    /*{
+        {
+          id: 'metro',
+          label: 'Metro',
+          data: metro.features,
+        },
+        {
+          id: 'santamonica',
+          label: 'Santa Monica',
+          data: Santa_Monica.features,
+        },
+        /*{
         id: 'assembly',
         label: 'Assembly',
         data: assembly.features,
@@ -57,7 +83,10 @@ export default function ContactLibrary({ recipients, setRecipients }) {
         label: 'Senate',
         data: senate.features,
       }*/
-  ];
+      ]);
+    }
+    loadContacts();
+  }, []);
 
   // TODO: Rewrite this whole componenet to injest from our API
   // const geodata = geoLoader(e, false);
@@ -68,11 +97,22 @@ export default function ContactLibrary({ recipients, setRecipients }) {
    */
   function addAll(data) {
     const updatedRecipients = data.flatMap((feature) => {
-      if (areDeputiesShown && feature.properties.Deputy) {
-        // Include deputy email if applicable
-        return [feature.properties.DEMAIL, feature.properties.Deputy];
+      if (feature.properties) {
+        // Raw feature from GeoJSON (old)
+        if (areDeputiesShown && feature.properties.Deputy) {
+          // Include deputy email if applicable
+          return [feature.properties.DEMAIL, feature.properties.Deputy];
+        } else {
+          return feature.properties.DEMAIL;
+        }
       } else {
-        return feature.properties.DEMAIL;
+        // Parsed contact from API (new)
+        if (areDeputiesShown && feature.secondaryEmail) {
+          // Include deputy email if applicable
+          return [feature.primaryEmailL, feature.secondaryEmail];
+        } else {
+          return feature.primaryEmail;
+        }
       }
     });
 
@@ -117,7 +157,7 @@ export default function ContactLibrary({ recipients, setRecipients }) {
         {/* flex flex-col overflow-auto - all the way down to make "sticky" header work*/}
         <Dialog.Content className="fixed top-1/2 left-1/2 z-10 flex max-h-[85vh] w-3xl max-w-screen -translate-x-1/2 -translate-y-1/2 flex-col overflow-auto border-2 bg-white">
           <Dialog.Close
-            className="absolute top-2.5 right-2.5 inline-flex size-[25px] items-center justify-center border-none p-0 text-2xl"
+            className="absolute top-2.5 right-2.5 inline-flex size-6.25 items-center justify-center border-none p-0 text-2xl"
             aria-label="Close"
           >
             <Icon icon="material-symbols:close" />
@@ -127,102 +167,146 @@ export default function ContactLibrary({ recipients, setRecipients }) {
             <Dialog.Title className="text-xl font-bold">
               Contact Library
             </Dialog.Title>
-
-            <Dialog.Description className="text-gray-400 italic">
-              NOTE: These have not been updated post-November 2024 election.
-            </Dialog.Description>
           </header>
 
           {/* Tabs */}
-          <Tabs.Root
-            defaultValue={categories[0].id}
-            className="flex flex-col overflow-auto"
-          >
-            <Tabs.List aria-label="Legislative body" className="z-10">
+          {categories.length ? (
+            <Tabs.Root
+              defaultValue={categories[0].id}
+              className="flex flex-col overflow-auto"
+              onValueChange={(test) => {
+                console.log(test);
+              }}
+            >
+              <Tabs.List aria-label="Legislative body" className="z-10">
+                {categories.map((category) => (
+                  <Tabs.Trigger
+                    key={category.id}
+                    value={category.id}
+                    className="border-2 border-black bg-white not-last:border-r-0 first:border-l-0 hover:bg-black data-[state=active]:cursor-auto data-[state=active]:border-b-white hover:data-[state=active]:bg-white hover:data-[state=active]:text-black"
+                  >
+                    {category.label}
+                  </Tabs.Trigger>
+                ))}
+              </Tabs.List>
+
+              {/* Content */}
               {categories.map((category) => (
-                <Tabs.Trigger
+                <Tabs.Content
                   key={category.id}
                   value={category.id}
-                  className="border-2 border-black bg-white not-last:border-r-0 first:border-l-0 hover:bg-black data-[state=active]:cursor-auto data-[state=active]:border-b-white hover:data-[state=active]:bg-white hover:data-[state=active]:text-black"
+                  className="-mt-0.5 flex flex-col overflow-auto border-t-2 border-black bg-white text-sm"
                 >
-                  {category.label}
-                </Tabs.Trigger>
-              ))}
-            </Tabs.List>
+                  {/* Toolbar */}
+                  <div className="flex items-center justify-between gap-8 border-b-2 p-4">
+                    <span className="grow italic">
+                      {category.updatedAt
+                        ? `Last updated ${new Date(
+                            category.updatedAt,
+                          ).toLocaleDateString('en-US')}
+                        `
+                        : 'NOTE: These have not been updated post-November 2024 election.'}
+                    </span>
 
-            {/* Content */}
-            {categories.map((category) => (
-              <Tabs.Content
-                key={category.id}
-                value={category.id}
-                className="-mt-0.5 flex flex-col overflow-auto border-t-2 border-black bg-white text-sm"
-              >
-                {/* Toolbar */}
-                <div className="flex justify-between border-b-2 p-4">
-                  <button className="" onClick={() => addAll(category.data)}>
-                    Add All
-                  </button>
+                    {/* Show deputies */}
+                    {category.id === 'metro' && (
+                      <div className="flex items-center gap-2.5 whitespace-nowrap">
+                        <Checkbox.Root
+                          id="deputies"
+                          checked={areDeputiesShown}
+                          onCheckedChange={() => {
+                            setAreDeputiesShown(!areDeputiesShown);
+                          }}
+                          className="flex size-6 items-center justify-center border-2 border-black p-0"
+                        >
+                          <Checkbox.Indicator>
+                            <Icon icon="material-symbols:check" />
+                          </Checkbox.Indicator>
+                        </Checkbox.Root>
+                        <label htmlFor="deputies">Show deputies</label>
+                      </div>
+                    )}
 
-                  {/* Show deputies */}
-                  {category.id === 'metro' && (
-                    <div className="flex items-center gap-2.5">
-                      <Checkbox.Root
-                        id="deputies"
-                        checked={areDeputiesShown}
-                        onCheckedChange={() => {
-                          setAreDeputiesShown(!areDeputiesShown);
-                        }}
-                        className="flex size-6 items-center justify-center border-2 border-black p-0"
-                      >
-                        <Checkbox.Indicator>
-                          <Icon icon="material-symbols:check" />
-                        </Checkbox.Indicator>
-                      </Checkbox.Root>
-                      <label htmlFor="deputies">Show deputies</label>
-                    </div>
-                  )}
-                </div>
-
-                {/* List */}
-                <table className="flex flex-col overflow-auto">
-                  <tbody>
-                    {category.data.map((feature, index) => {
-                      if (feature.properties.DEMAIL) {
-                        return (
-                          <tr
-                            key={index}
-                            data-email={feature.properties.DEMAIL}
-                            className="cursor-pointer leading-normal not-last:border-b-2 hover:bg-black hover:text-white"
-                            onClick={() => {
-                              updateRecipients(
-                                feature.properties.DEMAIL,
-                                feature.properties.Deputy,
-                              );
-                            }}
-                          >
-                            {feature.properties.District && (
+                    <button
+                      className="whitespace-nowrap"
+                      onClick={() => addAll(category.data)}
+                    >
+                      Add All
+                    </button>
+                  </div>
+                  {/* List */}
+                  <table className="flex flex-col overflow-auto">
+                    <tbody>
+                      {category.data.map((feature, index) => {
+                        if (feature.properties) {
+                          // Raw feature from GeoJSON (old)
+                          return (
+                            <tr
+                              key={index}
+                              data-email={feature.properties.DEMAIL}
+                              className="cursor-pointer leading-normal not-last:border-b-2 hover:bg-black hover:text-white"
+                              onClick={() => {
+                                updateRecipients(
+                                  feature.properties.DEMAIL,
+                                  feature.properties.Deputy,
+                                );
+                              }}
+                            >
+                              {feature.properties.District && (
+                                <td className="px-4 py-2 whitespace-nowrap">
+                                  {feature.properties.District}
+                                </td>
+                              )}
                               <td className="px-4 py-2 whitespace-nowrap">
-                                {feature.properties.District}
+                                {feature.properties.NAME}
                               </td>
-                            )}
-                            <td className="px-4 py-2 whitespace-nowrap">
-                              {feature.properties.NAME}
-                            </td>
-                            <td className="w-[99%] px-4 py-2">
-                              {feature.properties.DEMAIL}
-                            </td>
-                            <td className="px-4 py-2">
-                              {areDeputiesShown && feature.properties.Deputy}
-                            </td>
-                          </tr>
-                        );
-                      }
-                    })}
-                  </tbody>
-                </table>
-              </Tabs.Content>
-            ))}
-          </Tabs.Root>
+                              <td className="w-[99%] px-4 py-2">
+                                {feature.properties.DEMAIL}
+                              </td>
+                              <td className="px-4 py-2">
+                                {areDeputiesShown && feature.properties.Deputy}
+                              </td>
+                            </tr>
+                          );
+                        } else {
+                          // Parsed contact from API (new)
+                          return (
+                            <tr
+                              key={index}
+                              className="cursor-pointer leading-normal not-last:border-b-2 hover:bg-black hover:text-white"
+                              onClick={() => {
+                                updateRecipients(
+                                  feature.primaryEmail,
+                                  feature.secondaryEmail,
+                                );
+                              }}
+                            >
+                              {feature.title && (
+                                <td className="px-4 py-2 whitespace-nowrap">
+                                  {feature.title}
+                                </td>
+                              )}
+                              <td className="px-4 py-2 whitespace-nowrap">
+                                {feature.name}
+                              </td>
+                              <td className="w-[99%] px-4 py-2">
+                                {feature.primaryEmail}
+                              </td>
+                              {areDeputiesShown && (
+                                <td className="px-4 py-2">
+                                  {feature.secondaryEmail}
+                                </td>
+                              )}
+                            </tr>
+                          );
+                        }
+                      })}
+                    </tbody>
+                  </table>
+                </Tabs.Content>
+              ))}
+            </Tabs.Root>
+          ) : null}
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
---
+++ src/app/helpers/contacts.ts
@@ -0,0 +1,29 @@
+'use server';
+
+const GEODATA_API_BASE_URL = process.env.GEODATA_API_BASE_URL;
+
+async function getNeighborhoodCouncils() {
+  const data = await fetch(
+    `${GEODATA_API_BASE_URL}/v1/cities/los-angeles/neighborhood-councils?geom=false`,
+  );
+  const featureCollection = await data.json();
+
+  const contacts = featureCollection.features.map((feature) => {
+    return {
+      id: feature.id,
+      title: null,
+      name: feature.properties.name,
+      primaryEmail: feature.properties.contactDetails.find(
+        (detail) => detail.type === 'email',
+      ).value,
+      secondaryEmail: null,
+    };
+  });
+
+  return {
+    updatedAt: featureCollection.updatedAt,
+    contacts,
+  };
+}
+
+export { getNeighborhoodCouncils };
